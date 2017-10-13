fb37611b2ab2acb45923b58deb744d7f29498a00
Anass <anass.maai@gmail.com>
2017-10-13 08:46:04 -0400

    Version fonctionnel

--- LOG320-Lab2/src/PuzzleBox.java
+++ LOG320-Lab2/src/PuzzleBox.java
@@ -100,4 +100,6 @@ public class PuzzleBox {
 		}
 		return false;
 	}
+	
+	
 }
--- LOG320-Lab2/src/PuzzleMain.java
+++ LOG320-Lab2/src/PuzzleMain.java
@@ -3,6 +3,34 @@ public class PuzzleMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		GameEngine gE = new GameEngine("test.puzzle"); /*args[0]*/
 
+	        System.out.print("Jeu initial :\n");
+	        gE.printPuzzle();
+	        System.out.print("\n");
+
+	        double startTime = System.currentTimeMillis();
+	        double elaspedTime = 0;
+
+	        if(gE.resoudrePuzzle()){
+	            elaspedTime = (System.currentTimeMillis() - startTime)/1000;
+	        }else{
+	            elaspedTime = (System.currentTimeMillis() - startTime)/1000;
+	            System.out.print("\nJeu insolvable\n");
+	        }
+
+	        System.out.print("Nombre de noeuds parcourus: "+gE.getNodeCount()+"\n");
+	        System.out.print("Temps d'execution : " + elaspedTime + " secondes\n");
+	        System.out.print("Jeu final :\n");
+	        gE.printPuzzle();
+
+	        System.out.print("Solution :\n");
+
+	        for(Move desplacement : gE.getDeplacementList()){
+	            System.out.print("De ("+desplacement.getInitialej()+","+desplacement.getInitialei()+") ");
+	            System.out.print("à ("+desplacement.getFinalej()+","+desplacement.getFinalei()+")\n");
+	        }
+	    
 	}
+	
 }
